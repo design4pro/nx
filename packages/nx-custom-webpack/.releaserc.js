9f238ef2b9f0de7cb4e1d19fdcdff087b94b8893
Rafał Wolak <r.wolak@design4.pro>
2022-12-01 23:22:24 +0100

    fix(nx-custom-webpack): release fix

--- packages/nx-custom-webpack/.releaserc.js
+++ packages/nx-custom-webpack/.releaserc.js
@@ -1,7 +1,12 @@
-const { createReleaseConfigWithScopeFilter } = require('../../tools/release');
+const {
+  createReleaseConfigWithScopeFilter,
+} = require('@design4pro/nx-release');
+const options = require('../../.releaserc');
 
 module.exports = createReleaseConfigWithScopeFilter({
   projectScope: 'nx-custom-webpack',
   projectRoot: 'packages/nx-custom-webpack',
   buildOutput: 'dist/packages/nx-custom-webpack',
+  options,
+  changelogFile: 'CHANGELOG.md',
 });
