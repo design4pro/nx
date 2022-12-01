9dbececf8b62d6c1fb55b62aa2d724e04b261932
Rafał Wolak <r.wolak@design4.pro>
2022-12-01 23:24:10 +0100

    fix(nx-strapi): release fix

--- packages/nx-strapi/.releaserc.js
+++ packages/nx-strapi/.releaserc.js
@@ -1,7 +1,12 @@
-const { createReleaseConfigWithScopeFilter } = require('../../tools/release');
+const {
+  createReleaseConfigWithScopeFilter,
+} = require('@design4pro/nx-release');
+const options = require('../../.releaserc');
 
 module.exports = createReleaseConfigWithScopeFilter({
   projectScope: 'nx-strapi',
   projectRoot: 'packages/nx-strapi',
   buildOutput: 'dist/packages/nx-strapi',
+  options,
+  changelogFile: 'CHANGELOG.md',
 });
